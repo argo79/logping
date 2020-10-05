3115fb698e201bc32800d618412c4e79650f4144
root <melloni6@gmail.com>
2020-10-06 00:35:22 +0200

    Aggiornamento

--- logping.sh
+++ logping.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-date >> /home/argo/ping.txt ; ping -c 3 www.google.it >> /home/argo/ping.txt
-cat /home/argo/ping.txt | grep -E 'CEST|rtt|transmitted|Error' > /home/argo/logping.txt
-echo "Tentativi (CEST): " >> /home/argo/logping.txt ; cat /home/argo/logping.txt | grep -c CEST >> /home/argo/logping.txt
-echo "Riusciti (rtt): " >> /home/argo/logping.txt ; cat /home/argo/logping.txt | grep -c rtt >> /home/argo/logping.txt
-
+date >> /var/log/ping.txt ; ping -c 3 www.google.it >> /var/log/ping.txt
+cat /var/log/ping.txt | grep -E 'CEST|rtt|transmitted|Error' > /var/log/logping.txt
+echo "Tentativi (CEST): " >> /var/log/logping.txt ; cat /var/log/logping.txt | grep -c CEST >> /var/log/logping.txt
+echo "Riusciti (rtt): " >> /var/log/logping.txt ; cat /var/log/logping.txt | grep -c rtt >> /var/log/logping.txt

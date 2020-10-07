6173ace35cd4217b985e2d655de509c1b5225ef2
Argo79 <melloni6@gmail.com>
2020-10-07 19:00:18 +0200

    Aggiornamento

--- logping.sh
+++ logping.sh
@@ -41,10 +41,11 @@ echo -e "\e[1m\e[31m- Interruzioni \e[0m-" >> $dir/logping.log
 # echo "" >> $dir/logping.log
 cat $dir/ping.txt | grep -zPo '.*CEST\nSTOP\n' >> $dir/logping.log
 echo "" >> $dir/logping.log
-echo -e "\e[1m\e[95m- Ultimo tentativo -\e[0m" >> $dir/logping.log
+echo -e "\e[1m\e[95m- Ultimo tentativo -\e[0m\e[96m" >> $dir/logping.log
 cat $dir/logping.txt |grep  -zPo '.*CEST\n.*\nrtt.*\n' > $dir/lastping.log
 tail -n 4 $dir/lastping.log >> $dir/logping.log
-echo "" >> $dir/logping.log
+echo -e "\e[0m" >> $dir/logping.log
+# echo "" >> $dir/logping.log
 echo -e "\e[1m\e[32m- Totali -\e[0m" >> $dir/logping.log
 cat $dir/logping.txt | grep -A1 -E 'Tentativi|Riusciti|Interruzioni' >> $dir/logping.log
 echo -e "**************************" >> $dir/logping.log

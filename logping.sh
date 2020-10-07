982f4d0947e37d3e43b7890526c4056793a60509
Argo79 <melloni6@gmail.com>
2020-10-07 14:56:20 +0200

    Aggiornamento

--- logping.sh
+++ logping.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 # Visualizza i numeri di CEST e di rtt
 # cat logping.txt |grep -A 1 : |grep -v :
 # Visualizza SOLO i ping persi
 # cat ping.txt |grep -zPo '.*CEST\nSTOP\n'
 
-dir="$HOME/log/"
+dir="$HOME/log"
 
 if [ ! -d $dir ] 	
 then
@@ -32,7 +34,8 @@ echo "Differenza: " >> $dir/logping.txt ; echo $Diff >> $dir/logping.txt
 # echo "Tentativi (CEST): " >> $dir/logping.txt ; cat $dir/logping.txt | grep -c CEST >> $dir/logping.txt
 # echo "Riusciti (rtt): " >> $dir/logping.txt ; cat $dir/logping.txt | grep -c rtt >> $dir/logping.txt
 
-cat $dir/ping.txt | grep -zPo '.*CEST\nSTOP\n' > $dir/logping.log
+echo "- Interruzioni -" > $dir/logping.log
+cat $dir/ping.txt | grep -zPo '.*CEST\nSTOP\n' >> $dir/logping.log
 echo "- Totali -" >> $dir/logping.log
-cat $dir/logping.txt | grep -A 1 : >> $dir/logping.log
+cat $dir/logping.txt | grep -A1 -E 'Tentativi|Riusciti|Differenza' >> $dir/logping.log
 # cat $dir/logping.txt |grep -A 1 : |grep -v : >> $dir/logping.log

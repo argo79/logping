b9788ab047908184c9f50a51358c6b01d3130b22
Argo79 <melloni6@gmail.com>
2020-10-07 02:16:34 +0200

    Aggiornamento

--- logping.sh
+++ logping.sh
@@ -11,14 +11,22 @@ then
     echo "Quindi la creo..."
     echo $dir
     mkdir $dir
-    exit 9999 # die with error code 9999
+    # exit 9999 # die with error code 9999
 else 
 	echo "Directory " $dir " EXISTS." 
 fi
 date >> $dir/ping.txt ; ping -c 3 www.google.it >> $dir/ping.txt
 echo "STOP" >> $dir/ping.txt
 cat $dir/ping.txt | grep -E 'CEST|rtt|transmitted|Error' > $dir/logping.txt
-echo "Tentativi (CEST): " >> $dir/logping.txt ; cat $dir/logping.txt | grep -c CEST >> $dir/logping.txt
-echo "Riusciti (rtt): " >> $dir/logping.txt ; cat $dir/logping.txt | grep -c rtt >> $dir/logping.txt
+
+cat $dir/logping.txt | grep -c CEST > $dir/numeroCEST.txt 
+cat $dir/logping.txt | grep -c rtt > $dir/numeroRtt.txt
+
+echo "Tentativi (CEST): " >> $dir/logping.txt ; cat $dir/numeroCEST.txt >> $dir/logping.txt
+echo "Riusciti (rtt): " >> $dir/logping.txt ; cat $dir/numeroRtt.txt >> $dir/logping.txt
+
+# echo "Tentativi (CEST): " >> $dir/logping.txt ; cat $dir/logping.txt | grep -c CEST >> $dir/logping.txt
+# echo "Riusciti (rtt): " >> $dir/logping.txt ; cat $dir/logping.txt | grep -c rtt >> $dir/logping.txt
+
 cat $dir/ping.txt |grep -zPo '.*CEST\nSTOP\n' > $dir/logping.log
 cat $dir/logping.txt |grep -A 1 : |grep -v : >> $dir/logping.log

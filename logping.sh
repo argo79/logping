facab5280e6432b846804302033b8a380ef712ec
Argo79 <melloni6@gmail.com>
2020-10-07 15:36:19 +0200

    Aggiornamento

--- logping.sh
+++ logping.sh
@@ -34,11 +34,13 @@ echo "Differenza: " >> $dir/logping.txt ; echo $Diff >> $dir/logping.txt
 # echo "Tentativi (CEST): " >> $dir/logping.txt ; cat $dir/logping.txt | grep -c CEST >> $dir/logping.txt
 # echo "Riusciti (rtt): " >> $dir/logping.txt ; cat $dir/logping.txt | grep -c rtt >> $dir/logping.txt
 
-echo "- Interruzioni -" > $dir/logping.log
+echo "File log di logping.sh" > $dir/logping.log
+echo "***********************" >> $dir/logping.log
+echo "- Interruzioni -" >> $dir/logping.log
 echo "" >> $dir/logping.log
 cat $dir/ping.txt | grep -zPo '.*CEST\nSTOP\n' >> $dir/logping.log
 echo "" >> $dir/logping.log
 echo "- Totali -" >> $dir/logping.log
-echo "" >> $dir/logping.log
 cat $dir/logping.txt | grep -A1 -E 'Tentativi|Riusciti|Differenza' >> $dir/logping.log
+echo "***********************" >> $dir/logping.log
 # cat $dir/logping.txt |grep -A 1 : |grep -v : >> $dir/logping.log

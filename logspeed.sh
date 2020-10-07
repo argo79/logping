45b117be1f5cafce1e5c52b411668c27aef284b7
Argo79 <melloni6@gmail.com>
2020-10-07 20:16:43 +0200

    Aggiornamento

--- logspeed.sh
+++ logspeed.sh
@@ -2,4 +2,4 @@
 
 dir="$HOME/log"
 date >> $dir/speedtest.txt ; speedtest-cli >> $dir/speedtest.txt
-cat $dir/speedtest.txt | grep -E 'CEST|Download|Upload' > $dir/logspeed.txt
+cat $dir/speedtest.txt | grep -E 'CEST|Download|Upload' > $dir/logspeed.log
